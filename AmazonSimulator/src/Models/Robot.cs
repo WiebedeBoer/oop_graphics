8f3aad01db7735fd3cf94e237dae590e93c5c366
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-09-26 11:39:57 +0200

    move upd
    move

--- AmazonSimulator/src/Models/Robot.cs
+++ AmazonSimulator/src/Models/Robot.cs
@@ -5,26 +5,44 @@ using Newtonsoft.Json;
 
 namespace Models {
     public class Robot : C3model, IUpdatable {
-        public string status = "idle";
+        
+        private double tX, tY, tZ;
+
         public Robot (double x, double y, double z, double rotationX, double rotationY, double rotationZ) :base(x, y, z, rotationX, rotationY, rotationZ,"robot")
         {
             
         }
+
         public void Target (double x, double y, double z){
-            while(x!=this.x && y!=this.y && z!=this.z){
-                if(x>this.x) {
-                    Move(this.x+0.1,this.y,this.z);
-                    needsUpdate = true;
-                }
-            }
+            tX = x;
+            tY = y;
+            tZ = z;
+
+            //while(x!=this.x && y!=this.y && z!=this.z){
+                // if(x>this.x) {
+                //     Move(this.x+0.1,this.y,this.z);
+                //     needsUpdate = true;
+                // }
+            //}
         }
         public override bool Update(int tick)
         {
-            if(needsUpdate) {
-                needsUpdate = false;
-                return true;
-            }
-            return false;
+            //if(tX!=this.x && tY!=this.y && tZ!=this.z) {
+                //move
+            //}
+               if(x>this.x) {
+                     Move(this.x+0.1,this.y,this.z);
+                     needsUpdate = true;
+                }
+
+            //this.Target(this.x+0.1,this.y,this.z);
+            return base.Update(tick);
+            
+            //if(needsUpdate) {
+            //    needsUpdate = false;
+            //    return true;
+            //}
+            //return false;
         }
     }
 }
