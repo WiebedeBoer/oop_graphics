722881518d2c0a5527bfed4055fd66975db8d2bc
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-09-26 11:54:04 +0200

    redo move
    redo move

--- AmazonSimulator/src/Models/Robot.cs
+++ AmazonSimulator/src/Models/Robot.cs
@@ -14,28 +14,16 @@ namespace Models {
         }
 
         public void Target (double x, double y, double z){
-            tX = x;
-            tY = y;
-            tZ = z;
-
             //while(x!=this.x && y!=this.y && z!=this.z){
-                // if(x>this.x) {
-                //     Move(this.x+0.1,this.y,this.z);
-                //     needsUpdate = true;
-                // }
+                if(x>this.x) {
+                    Move(this.x+0.1,this.y,this.z);
+                    needsUpdate = true;
+                }
             //}
         }
         public override bool Update(int tick)
         {
-            //if(tX!=this.x && tY!=this.y && tZ!=this.z) {
-                //move
-            //}
-               if(x>this.x) {
-                     Move(this.x+0.1,this.y,this.z);
-                     needsUpdate = true;
-                }
-
-            //this.Target(this.x+0.1,this.y,this.z);
+            this.Target(this.x+0.1,this.y,this.z);
             return base.Update(tick);
             
             //if(needsUpdate) {
