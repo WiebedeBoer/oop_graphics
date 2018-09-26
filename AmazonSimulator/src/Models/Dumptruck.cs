3319e67db3caa7787f3539fee17a17895c64b168
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-09-26 13:04:36 +0200

    truck move z as
    truck move z as

--- AmazonSimulator/src/Models/Dumptruck.cs
+++ AmazonSimulator/src/Models/Dumptruck.cs
@@ -6,17 +6,36 @@ using Newtonsoft.Json;
 namespace Models {
     public class Dumptruck : C3model, IUpdatable {
         
+        private double tX, tY, tZ;
         public Dumptruck (double x, double y, double z, double rotationX, double rotationY, double rotationZ) :base(x, y, z, rotationX, rotationY, rotationZ,"dumptruck")
         {
             
         }
-        public virtual bool Update(int tick)
+
+        public void Target (double x, double y, double z){
+            tX = x;
+            tY = y;
+            tZ = z;
+
+            if(tX!=this.x || tY!=this.y || tZ!=this.z) {
+                Move(this.x,this.y,this.z-0.02);
+                needsUpdate = true;
+            }
+
+        }
+        public override bool Update(int tick)
         {
-            if(needsUpdate) {
-                needsUpdate = false;
-                return true;
+            if(tX!=this.x || tY!=this.y || tZ!=this.z) {
+            this.Move(this.x,this.y,this.z-0.02);
+            return base.Update(tick);
             }
             return false;
+            
+            //if(needsUpdate) {
+            //    needsUpdate = false;
+            //    return true;
+            //}
+            //return false;
         }
     }
 }
