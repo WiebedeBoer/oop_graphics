05c7058c7c257f7f21c9ac8205af8a7ed3dfba9d
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-03 10:12:17 +0200

    bool cargoed
    bool cargoed

--- AmazonSimulator/src/Models/Kast.cs
+++ AmazonSimulator/src/Models/Kast.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 namespace Models {
     public class Kast : C3model, IUpdatable {
         public string opgeslagenLocatie;
+        public bool Cargoed;
         
         public Kast (double x, double y, double z, double rotationX, double rotationY, double rotationZ, string opgeslagenLocatie, bool Cargoed) :base(x, y, z, rotationX, rotationY, rotationZ,"kast")
         {
