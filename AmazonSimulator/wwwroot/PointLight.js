c194d2a37889c3f69ab238781873c56d578cda6b
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-10-05 11:59:36 +0200

    intensity fix
    intensity fix

--- AmazonSimulator/wwwroot/PointLight.js
+++ AmazonSimulator/wwwroot/PointLight.js
@@ -1,4 +1,4 @@
-function addPointLight(object,color,x,y,z,distance){
+function addPointLight(object,color,x,y,z,intensity,distance){
     var light = new THREE.PointLight(color, intensity, distance);
     light.position.set(x,y,z);
     object.add(light);
